36e187b6fcc508f0f87b5ae5756821d0f1e117b7
Amimul Ehshan <amimulehshan29@gmail.com>
2021-06-22 14:15:31 +0600

    refactor white spaces

--- app/Console/Commands/UnitTestHelper.php
+++ app/Console/Commands/UnitTestHelper.php
@@ -46,25 +46,15 @@ class UnitTestHelper{
     }
 
     public function addNameSpace($nameSpace){
-        $this->output.='
-namespace '.$nameSpace.';
-
-';
+        $this->output.=PHP_EOL.'namespace '.$nameSpace.';'.PHP_EOL.PHP_EOL;
     }
 
     public function addRequires($file){
-        $this->output.='require \'vendor/autoload.php\';
-require \''.$file.'\';';
+        $this->output.='require \'vendor/autoload.php\';'.PHP_EOL.'require \''.$file.'\';';
     }
 
     public function addUses(){
-        $this->output.='
-        
-use '.implode('\\',$this->object->name->parts).'\\'.$this->class->name->name.';
-use ReflectionClass;
-use PHPUnit\Framework\TestCase;
-
-';
+        $this->output.=PHP_EOL.PHP_EOL.'use '.implode('\\',$this->object->name->parts).'\\'.$this->class->name->name.';'.PHP_EOL.'use ReflectionClass;'.PHP_EOL.'use PHPUnit\Framework\TestCase;'.PHP_EOL.PHP_EOL;
     }
 
     public function addClassname(){
@@ -72,118 +62,73 @@ use PHPUnit\Framework\TestCase;
     }
 
     public function addClassObject(){
-        $this->output.='{
-    protected $'.lcfirst($this->class->name->name).';';
+        $this->output.='{'.PHP_EOL."\t".'protected $'.lcfirst($this->class->name->name).';';
     }
 
     public function addProperties(){
         foreach ($this->properties as $property){
-            $this->output.='
-    protected $'.$property->props[0]->name->name.';';
+            $this->output.=PHP_EOL."\t".'protected $'.$property->props[0]->name->name.';';
         }
     }
     public function addSetUp(){
-        $this->output.='
-        
-    protected function setUp(): void
-    {
-        parent::setUp();';
+        $this->output.=PHP_EOL.PHP_EOL."\t".'protected function setUp(): void'.PHP_EOL."\t".'{'.PHP_EOL."\t".'parent::setUp();';
         foreach ($this->properties as $property){
-            $this->output.='
-        $this->'.$property->props[0]->name->name.'=\'\'; //TODO set test value';
+            $this->output.=PHP_EOL."\t\t".'$this->'.$property->props[0]->name->name.'=\'\'; //TODO set test value';
         }
     }
 
     public function addConstructedObject(){
-        $this->output.='
-        $this->'.lcfirst($this->class->name->name).' = new '.$this->class->name->name.'('.$this->constructorParams.');
-    }';
+        $this->output.="\n\t\t".'$this->'.lcfirst($this->class->name->name).' = new '.$this->class->name->name.'('.$this->constructorParams.');'."\n\t".'}';
     }
 
     public function addTearDownMethod(){
-        $this->output.='
-        
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        unset($this->'.lcfirst($this->class->name->name).');';
+        $this->output.="\n\n\t".'protected function tearDown(): void'."\n\t".'{'."\n\t\t".'parent::tearDown();'."\n\n\t\t".'unset($this->'.lcfirst($this->class->name->name).');';
         foreach ($this->properties as $property){
-            $this->output.='
-        unset($this->'.$property->props[0]->name->name.');';
+            $this->output.="\n\t\t".'unset($this->'.$property->props[0]->name->name.');';
         }
-
-        $this->output.='
+        $this->output.="\n\t".'}'."\n\n";
     }
 
-';
-    }
+    public function addMethods($testMethods,$method){
 
-    public function addMethods(){
-        $testMethods='';
-
-        foreach ($this->methods as $method){
-            $prprty='';
-            $asrt='';
-            if (@$method->returnType->name=='void'){
-                $params=$method->params;
-                $paramNames=[];
-                $getProp=[];
-                $getAssert=[];
-                $prprty='';
-                if (sizeof($params)>0) {
-                    foreach($params as $key=>$param){
-                        $paramName='$'.$param->var->name;
-                        array_push($paramNames,$paramName);
-                        $prprty.=$paramName.'=\'\'; //TODO set test value
-        ';
-                        $prop='
-        $property'.$key.' = (new ReflectionClass('.$this->class->name->name.'::class))
-            ->getProperty(\''.$param->var->name.'\');
-        $property'.$key.'->setAccessible(true);';
-                        array_push($getProp,$prop);
-                        $asrt='$this->assertSame('.$paramName.', $property'.$key.'->getValue($this->'.lcfirst($this->class->name->name).'));
-        ';
-                        array_push($getAssert,$asrt);
-                    }
+        $prprty='';
+        $asrt='';
+        if (@$method->returnType->name=='void'){
+            $params=$method->params;
+            $paramNames=[];
+            $getProp=[];
+            $getAssert=[];
+            if (sizeof($params)>0) {
+                foreach($params as $key=>$param){
+                    $paramName='$'.$param->var->name;
+                    array_push($paramNames,$paramName);
+                    $prprty.=$paramName.'=\'\'; //TODO set test value'."\n\t\t";
+                    $prop="\n\t\t".'$property'.$key.' = (new ReflectionClass('.$this->class->name->name.'::class))'."\n\t\t\t".'->getProperty(\''.$param->var->name.'\');'."\n\t\t".'$property'.$key.'->setAccessible(true);';
+                    array_push($getProp,$prop);
+                    $asrt='$this->assertSame('.$paramName.', $property'.$key.'->getValue($this->'.lcfirst($this->class->name->name).'));'."\n\t\t";
+                    array_push($getAssert,$asrt);
                 }
-                $proprty='$this->'.lcfirst($this->class->name->name).'->'.$method->name->name.'('.implode(',', $paramNames).');';
-                $testMethods.='public function test'.ucfirst($method->name->name).'(): void
-    {
-        '.$prprty.'
-        '.$proprty.'
-        '.implode('',$getProp).'
-        '.implode('',$getAssert).'
-    }
-
-';
-            } else{
-                $params=$method->params;
-                $paramNames=[];
-                $prprty='';
-                if (sizeof($params)>0) {
-                    foreach($params as $param){
-                        $paramName='$'.$param->var->name;
-                        array_push($paramNames,$paramName);
-                        $prprty.=$paramName.'=\'\'; //TODO set test value
-        ';
-                    }
-                }
-                $paramNames=implode(',', $paramNames);
-                $asrt='$this->assertSame($expected, $this->'.lcfirst($this->class->name->name).'->'.$method->name->name.'('.$paramNames.'));';
-                $testMethods.='    public function test'.ucfirst($method->name->name).'(): void
-    {
-        $expected = \'\';//TODO set test value
-        '.$prprty.'
-        '.$asrt.'
-    }
-
-';
             }
-
+            $proprty='$this->'.lcfirst($this->class->name->name).'->'.$method->name->name.'('.implode(',', $paramNames).');';
+            $testMethods.="\t".'public function test'.ucfirst($method->name->name).'(): void'."\n\t".'{'."\n\t\t".$prprty."\n\t\t".$proprty."\n\t\t".implode('',$getProp)."\n\t\t".implode('',$getAssert)."\n\t".'}'."\n\n";
+        } else{
+            $params=$method->params;
+            $paramNames=[];
+            if (sizeof($params)>0) {
+                foreach($params as $param){
+                    $paramName='$'.$param->var->name;
+                    array_push($paramNames,$paramName);
+                    $prprty.=$paramName.'=\'\'; //TODO set test value'."\n\t\t";
+                }
+            }
+            $paramNames=implode(',', $paramNames);
+            $asrt='$this->assertSame($expected, $this->'.lcfirst($this->class->name->name).'->'.$method->name->name.'('.$paramNames.'));';
+            $testMethods.="\t".'public function test'.ucfirst($method->name->name).'(): void'."\n\t".'{'."\n\t\t".'$expected = \'\';//TODO set test value'."\n\t\t".$prprty."\n\t\t".$asrt."\n\t".'}'."\n\n";
         }
-        $this->output.=$testMethods.'}
-';
+        return $testMethods;
+    }
+    public function closeClass(){
+        $this->output.='}';
     }
 
 
